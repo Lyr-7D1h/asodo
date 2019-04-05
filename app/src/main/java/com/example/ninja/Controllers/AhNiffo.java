e693b62746849f70414508f55abd712bf53a905f
Timberto <info@timberto.nl>
2019-04-05 20:51:29 +0200

    Improve paasei

--- app/src/main/java/com/example/ninja/Controllers/AhNiffo.java
+++ app/src/main/java/com/example/ninja/Controllers/AhNiffo.java
@@ -11,12 +11,13 @@ import android.widget.Button;
 import com.example.ninja.Controllers.Routetracking.Route;
 import com.example.ninja.Controllers.Routetracking.Startroute;
 import com.example.ninja.Controllers.Stats.HistoryList;
+import com.example.ninja.Controllers.abstractActivities.BackButtonActivity;
 import com.example.ninja.Domain.Global;
 import com.example.ninja.Domain.util.ActivityUtils;
 
 import com.example.ninja.R;
 
-public class AhNiffo extends AppCompatActivity {
+public class AhNiffo extends BackButtonActivity {
 
     private final Context context = this;
 
