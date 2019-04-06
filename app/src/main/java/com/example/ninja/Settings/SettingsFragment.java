9e6dd6fcb9e184c94daef6ea941ee585a7e9bb7e
Timberto <info@timberto.nl>
2019-04-06 17:13:46 +0200

    Add preferences

--- app/src/main/java/com/example/ninja/Settings/SettingsFragment.java
+++ app/src/main/java/com/example/ninja/Settings/SettingsFragment.java
@@ -4,7 +4,11 @@ package com.example.ninja.Settings;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import android.support.v7.preference.EditTextPreference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.text.InputType;
+import android.text.method.DigitsKeyListener;
+import android.widget.EditText;
 
 import com.example.ninja.R;
 
@@ -14,6 +18,5 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle bundle, String s) {
         setPreferencesFromResource(R.xml.preferences, s);
     }
-
 
 }
