547611980a1d1eaa06105e9f8ae628648a4e9337
Timberto <info@timberto.nl>
2019-04-06 17:15:23 +0200

    Add back button functionality

--- app/src/main/java/com/example/ninja/Settings/SettingsActivity.java
+++ app/src/main/java/com/example/ninja/Settings/SettingsActivity.java
@@ -4,9 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import com.example.ninja.Controllers.abstractActivities.BackButtonActivity;
 import com.example.ninja.R;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends BackButtonActivity {
 
     private Toolbar toolbar;
 
